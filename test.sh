75678d89d5295d97a192fd5eee7b776347f9799e
ww979951807 <59465186+ww979951807@users.noreply.github.com>
2024-10-31 20:29:53 +0800

    one

--- test.sh
+++ test.sh
@@ -4,6 +4,9 @@
          apt-get update
          apt-get install -y ruby ruby-dev build-essential rpm
          gem install --no-document fpm
-apt-get install docker-ce docker-ce-cli containerd.io
-systemctl start docker
-docker run hello-world
+apt-get install -y ca-certificates lsb-release curl gnupg software-properties-common
+curl -fsSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] http://mirrors.aliyun.com/docker-ce/linux/ubuntu xenial stable"
+apt-get update
+apt-get install -y docker-ce
+docker --version
