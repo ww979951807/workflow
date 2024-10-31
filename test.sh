e4a0aba9c6c158c67cebf8ade795e993389cd539
ww979951807 <59465186+ww979951807@users.noreply.github.com>
2024-10-31 20:24:04 +0800

    one

--- test.sh
+++ test.sh
@@ -4,9 +4,6 @@
          apt-get update
          apt-get install -y ruby ruby-dev build-essential rpm
          gem install --no-document fpm
-apt-get install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update
-apt-get install docker-ce
+apt-get install docker-ce docker-ce-cli containerd.io
+systemctl start docker
 docker run hello-world
